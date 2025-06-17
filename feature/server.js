363292a339e26a4f4de8edab6f44906ebf108516
statova-nadejda <statovanadejds@gmail.com>
2025-06-18 02:16:36 +0300

    feat(api): add basic route placeholder for POST /api/optimize

--- feature/server.js
+++ feature/server.js
@@ -1,19 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 import apiRoutes from "./api/optimize/routes.js";
-
 
 const app = express();
 const domain = `http://localhost`;
 const port = 3000;
 
-
 app.use(bodyParser.json());
 
-
 app.use("/api/optimize", apiRoutes);
-
 
 app.listen(port, ()=> console.log(`Server running on port: ${domain}:${port}`));
