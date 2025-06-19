775843e1dce723c16243e43103b36b76700e0342
VICTUS\User <borisidasa@gmail.com>
2025-06-20 01:41:34 +0300

    feat: added ci/cd pipeline

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.use(bodyParser.json());
 
 app.use("/api/", router);
 
-app.listen(`${domain}:${port}`, () =>
+app.listen(`${port}`, () =>
   console.log(`Server running on port: ${domain}:${port}`)
 );
