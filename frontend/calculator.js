069b0b1150367b9e83b2bebf74e553659ad9997a
CavarnaliA <nastea.cavarnali@gmail.com>
2025-06-19 15:41:27 +0300

    fix: update calculator logic

--- frontend/calculator.js
+++ frontend/calculator.js
@@ -1,33 +1,22 @@
-
-const display = document.getElementById('display');
-
-function append(value) {
-  if (display.value === '0') {
-    display.value = value;
-  } else {
-    display.value += value;
-  }
-}
-
-
-function clearDisplay() {
-  display.value = '0';
-}
-
-
 function calculate() {
   try {
-    display.value = eval(display.value
-      .replace(/π/g, Math.PI)
-      .replace(/e/g, Math.E)
-      .replace(/√/g, 'Math.sqrt')
-      .replace(/ln/g, 'Math.log')
-      .replace(/log/g, 'Math.log10')
-      .replace(/sin/g, 'Math.sin')
-      .replace(/cos/g, 'Math.cos')
-      .replace(/tan/g, 'Math.tan')
-      .replace(/EXP/g, 'Math.exp')
-    );
+    let expr = display.value;
+    expr = expr.replace(/(\d+|\([^()]+\))!/g, 'factorial($1)');
+    expr = expr
+      .replace(/π/g, `(${Math.PI})`)
+      .replace(/e/g, `(${Math.E})`)
+      .replace(/√\(/g, 'Math.sqrt(')
+      .replace(/ln\(/g, 'Math.log(')
+      .replace(/log\(/g, 'Math.log10(')
+      .replace(/sin\(/g, 'Math.sin(')
+      .replace(/cos\(/g, 'Math.cos(')
+      .replace(/tan\(/g, 'Math.tan(')
+      .replace(/EXP/g, 'Math.exp(')
+      .replace(/%/g, '*0.01')
+      .replace(/x/g, '0')
+      .replace(/f\(x\)/g, '0');
+
+    display.value = new Function('factorial', `return ${expr}`)(factorial);
   } catch {
     display.value = 'Ошибка';
   }
